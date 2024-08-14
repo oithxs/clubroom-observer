fab880ae1792ddeae7bae140b70ce1ab6da85e50
e1q23079 <156163994+e1q23079@users.noreply.github.com>
2024-08-15 00:34:04 +0900

    discordBot

---
+++ DiscordBot0814.py
@@ -0,0 +1,60 @@
+import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
+import asyncio
+#環境変数の用意
+load_dotenv() # .envファイルの読み込み
+TOKEN = os.getenv("TOKEN")
+GUILD_ID = int(os.getenv("GUILD_ID"))
+ROLE_ID = int(os.getenv("ROLE_ID"))
+
+# BotのプレフィックスとIntentsを設定
+intents = discord.Intents.default()
+intents.members = True
+client = discord.Client(intents=intents)
+
+# Botの起動時に実行される処理
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user.name}')
+    guild = client.get_guild(GUILD_ID)
+    mes = guild.members
+    while True:
+        try:
+            #入室
+            with open("enter.txt","r+") as f:
+                enter = f.readlines()
+                f.truncate(0) #ファイルサイズを0にする
+            for username in enter:
+                username = username.strip() #空白文字を消す
+                for individual in mes:
+                    #print(individual.name)
+                    if(individual.name==username):
+                        role = guild.get_role(ROLE_ID)
+                        print(f"ADD_ROLE>>{username}")
+                        await individual.add_roles(role)
+            
+            #退室
+            with open("leave.txt","r+") as f:
+                enter = f.readlines()
+                f.truncate(0) #ファイルサイズを0にする
+            for username in enter:
+                username = username.strip() #空白文字を消す
+                for individual in mes:
+                    #print(individual.name)
+                    if(individual.name==username):
+                        role = guild.get_role(ROLE_ID)
+                        print(f"REMOVE_ROLE>>{username}")
+                        await individual.remove_roles(role)
+                
+            
+            print("CHECK")
+            #time.sleep(60)
+            await asyncio.sleep(60)
+          
+        except Exception as e:
+            print(f"error:{e}")
+    
+# Botを起動
+client.run(TOKEN)
--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # clubroom-observer
 かけたよ
 てすとてすとああああああ
+
+
+DiscordBot.py
+====================
+仕様
+・DiscordBot.pyを動かすときに追加で必要なライブラリ
+pip install python-dotenv
+※WARNINGは無視する
+pip install discord.py ライブラリ：
