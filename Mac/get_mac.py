fa1dbdc25305f9e148fca97f7c9adc59ad33506e
masaki0087 <136552609+masaki0087@users.noreply.github.com>
2024-08-15 14:26:25 +0900

    get_mac.pyの作成

--- Mac/get_mac.py
+++ Mac/get_mac.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
 import os
 import time
@@ -8,9 +7,11 @@ import time
 url = "http://150.89.253.11/"
 tplink_passwd = "takakun"
 
+options = webdriver.ChromeOptions()
+# options.add_argument("--force-device-scale-factor=0.5")
 driver = webdriver.Remote(
     command_executor = os.environ["SELENIUM_URL"],
-    options = webdriver.ChromeOptions()
+    options = options
 )
 
 driver.implicitly_wait(5)
@@ -23,6 +24,19 @@ except WebDriverException:
     driver.quit()
 
 else:
-    time.sleep(5)
+    text_box = driver.find_element(by=By.ID, value="pc-login-password")
+    button = driver.find_element(By.ID, value="pc-login-btn")
+    text_box.send_keys(tplink_passwd)
+    button.click()
+
+    #MACアドレスを取得
+    # driver.find_element(By.ID, value="map_wireless").click()
+    driver.find_element(By.ID, value="map_wire").click()
+    macaddr = driver.find_elements(by=By.XPATH, value="//*[@id='bodyWireStat']/tr/td[4]")
+
+    for elem in macaddr:
+        print(elem.text)
+
+    time.sleep(3)
     driver.close()
     driver.quit()
