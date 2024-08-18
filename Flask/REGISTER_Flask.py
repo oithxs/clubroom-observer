26f65a3a1e79d6f262b0012cd933c4f3a443413d
e1q23079 <156163994+e1q23079@users.noreply.github.com>
2024-08-18 23:34:34 +0900

    Final version

--- Flask/REGISTER_Flask.py
+++ Flask/REGISTER_Flask.py
@@ -1,8 +1,6 @@
 from flask import Flask, request, render_template
 import sqlite3
-
 from scapy.all import ARP, Ether, srp
-
 
 app = Flask(__name__)
 
@@ -11,9 +9,23 @@ app = Flask(__name__)
 # すでに存在していれば、それにアスセスする。
 dbname = 'USERTABLE.db'
 
+#MACアドレスがデータベースに存在するかどうかをTuleかFalseで返す。
+def Check_Mac_address(mac):
+    conn = sqlite3.connect(dbname)
+    cur = conn.cursor()
+
+    cur.execute("SELECT COUNT(*) FROM userdata WHERE MAC_ADDRESS = ?",(mac,))
+    result = cur.fetchone()[0]
+    
+    conn.close()
+
+    if result>0:
+        return True
+    else:
+        return False       
+
 #MACアドレスに変換
-#
-def IPtoMAcAddress(IPAddress):
+def IPtoMACAddress(IPAddress):
     # ARPリクエストパケットを作成
     arp = ARP(pdst=IPAddress)
     ether = Ether(dst="ff:ff:ff:ff:ff:ff")
@@ -39,20 +51,35 @@ def db_insert(DisUserName,MACAddress):
     # データベースへのコネクションを閉じる。(必須)
     conn.close()
 
+#Topページ
 @app.route("/")
 def top():
     return render_template("index.html")
 
+#登録[POST]
 @app.route("/store", methods=["POST"])
 def result():
+    
     DisUsName = request.form["DisUsName"]
     IPAddress = request.remote_addr
-    MACAddress = IPtoMAcAddress(IPAddress)
-    print(f"STORE>> UserName:{DisUsName} IPAddress:{IPAddress} MACAddress:{MACAddress}")
-    db_insert(DisUsName,MACAddress)
-    return render_template("store.html",DisUsName=DisUsName)
+    MACAddress = IPtoMACAddress(IPAddress)
+    
+    print(f"ACCESS FROM >> UserName:{DisUsName} IPAddress:{IPAddress} MACAddress:{MACAddress}")
+    
+    print(f"結果:{Check_Mac_address(MACAddress)}")
+    
+    if(Check_Mac_address(MACAddress)):
+        print("Already registered MAC address")
+        return render_template("error.html",error="すでに登録されています.")
+    elif(MACAddress==None):
+        print("Failed to obtain MAC address")
+        return render_template("error.html",error="MACアドレスの取得に失敗しました.")
+    else:
+        db_insert(DisUsName,MACAddress) #データベースへ登録
+        print("REGISTER OK")
+        return render_template("store.html",DisUsName=DisUsName)
 
 if __name__ == '__main__':
     print("REGISTER_SERVER_RUNNING")
-    app.debug = True
+    #app.debug = True
     app.run(host="0.0.0.0")
